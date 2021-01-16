b70d07b7f959d907e9a122dae2ef43371c09586e
ms-online <27732357@qq.com>
2021-01-16 17:50:39 +0800

    lesson-35

--- app.js
+++ app.js
@@ -1,124 +1,44 @@
 class Node {
-  constructor(value, parentNode = null) {
-    this.children = []
+  constructor(value) {
     this.value = value
-    this.parent = parentNode
+    this.right = null
+    this.left = null
   }
 
-  addNode(value) {
-    const segments = value.split('/')
-    if (segments.length === 0) {
+  add(value) {
+    if (this.value === null) {
+      this.value = value
       return
     }
-    if (segments.length === 1) {
-      const node = new Node(value, this)
-      this.children.push(node)
-      return { node: node, index: this.children.length - 1 }
-    }
 
-    const existingChildNode = this.children.find(
-      (child) => child.value === segments[0]
-    )
-
-    if (existingChildNode) {
-      existingChildNode.addNode(segments.slice(1).join('/'))
-    } else {
-      const node = new Node(segments[0], this)
-      node.addNode(segments.slice(1).join('/'))
-      this.children.push(node)
-      return { node: node, index: this.children.length - 1 }
-    }
-  }
-
-  removeNode(value) {
-    const segments = value.split('/')
-    if (segments.length === 0) {
+    if (this.value < value) {
+      const newNode = new Node(value)
+      this.right = newNode
       return
     }
-    if (segments.length === 1) {
-      const existingChildNode = this.children.findIndex(
-        (child) => child.value === segments[0]
-      )
-      if (existingChildNode < 0) {
-        throw new Error('无法找到匹配的值！')
-      }
-      this.children.splice(existingChildNode, 1)
-    }
-    if (segments.length > 1) {
-      const existingChildNode = this.children.find(
-        (child) => child.value === segments[0]
-      )
-      if (!existingChildNode) {
-        throw new Error('无法找到匹配的路径！路径为：' + segments[0])
-      }
-      existingChildNode.removeNode(segments.slice(1).join('/'))
-    }
-  }
 
-  // find(value) {
-  //   console.log(this)
-  //   //深度优先搜索
-  //   for (const child of this.children) {
-  //     if (child.value === value) {
-  //       return child
-  //     }
-  //     const nestedChildNode = child.find(value)
-  //     if (nestedChildNode) {
-  //       return nestedChildNode
-  //     }
-  //   }
-  // }
-  find(value) {
-    console.log(this)
-    //广度优先搜索
-    for (const child of this.children) {
-      if (child.value === value) {
-        return child
-      }
-    }
-    for (const child of this.children) {
-      const nestedChildNode = child.find(value)
-      if (nestedChildNode) {
-        return nestedChildNode
-      }
+    if (this.value > value) {
+      const newNode = new Node(value)
+      this.left = newNode
+      return
     }
   }
 }
 
 class Tree {
-  constructor(rootValue) {
-    this.root = new Node(rootValue)
+  constructor() {
+    this.root = new Node(null)
   }
-  add(path) {
-    this.root.addNode(path)
-  }
-  remove(path) {
-    this.root.removeNode(path)
-  }
-  find(value) {
-    if (this.root.value === value) {
-      return this.root
-    }
 
+  add(value) {
+    this.root.add(value)
+  }
+
+  remove(value) {
+    this.root.remove(value)
+  }
+
+  find(value) {
     return this.root.find(value)
   }
 }
-
-const filesystem = new Tree('/')
-// const pagesDocumentNode = filesystem.root.addNode('/pages文稿')
-// const deskTopNode = filesystem.root.addNode('/桌面')
-
-// pagesDocumentNode.node.addNode('/学习')
-// pagesDocumentNode.node.addNode('/工作')
-// deskTopNode.node.addNode('数据结构.key')
-filesystem.add('pages文稿/学习/前端学习路线.pdf')
-filesystem.add('pages文稿/工作/code.js')
-filesystem.add('下载/ps.dmg/ps.exe')
-filesystem.add('下载/ps.dmg/ps.txt')
-filesystem.remove('pages文稿/工作/code.js')
-filesystem.remove('下载/ps.dmg/ps.txt')
-// filesystem.remove('pages文稿/工作1/code.js')
-
-console.log(filesystem.find('ps.exe'))
-
-console.log(filesystem)
