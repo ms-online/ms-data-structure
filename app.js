227b631b79f9a83650e9018573c504b4433b3362
ms-online <27732357@qq.com>
2021-01-16 18:25:27 +0800

    lesson-36

--- app.js
+++ app.js
@@ -12,15 +12,37 @@ class Node {
     }
 
     if (this.value < value) {
+      if (this.right) {
+        this.right.add(value)
+        return
+      }
       const newNode = new Node(value)
       this.right = newNode
       return
     }
 
     if (this.value > value) {
+      if (this.left) {
+        this.left.add(value)
+        return
+      }
       const newNode = new Node(value)
       this.left = newNode
       return
+    }
+  }
+
+  find(value) {
+    if (this.value === value) {
+      return this
+    }
+
+    if (this.value < value && this.right) {
+      return this.right.find(value)
+    }
+
+    if (this.value > value && this.left) {
+      return this.left.find(value)
     }
   }
 }
@@ -42,3 +64,18 @@ class Tree {
     return this.root.find(value)
   }
 }
+
+const tree = new Tree()
+
+tree.add(10)
+tree.add(5)
+tree.add(2)
+tree.add(7)
+tree.add(15)
+tree.add(25)
+tree.add(30)
+
+console.log(tree)
+console.log(tree.find(7))
+console.log(tree.find(30))
+console.log(tree.find(11))
