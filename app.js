6f703035a77c8f9474918e98eadf874a531960ca
ms-online <27732357@qq.com>
2021-01-18 16:22:06 +0800

    lesson-53

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 class Node {
-  constructor(value) {
+  constructor(value, priority) {
     this.value = value
     this.next = null
+    this.priority = priority
   }
 }
 
@@ -9,28 +10,37 @@ class PriorityQueue {
   constructor() {
     this.first = null
   }
-  //3,4,5
-  insert(value) {
-    const newNode = new Node(value)
+  //3(1),4(8),5(10)
+  insert(value, priority) {
+    const newNode = new Node(value, priority)
 
-    if (!this.first) {
+    if (!this.first || priority > this.first.priority) {
+      newNode.next = this.first
       this.first = newNode
     } else {
       let currentNode = this.first
 
-      while (currentNode.next) {
+      while (currentNode.next && priority < currentNode.next.priority) {
         currentNode = currentNode.next
       }
-
+      //3(1),4(8), 6(9), 5(10)
+      newNode.next = currentNode.next
       currentNode.next = newNode
     }
+  }
+  process() {
+    const first = this.first
+    this.first = this.first.next
+    return first
   }
 }
 
 const priorityQueue = new PriorityQueue()
 
-priorityQueue.insert(3)
-priorityQueue.insert(4)
-priorityQueue.insert(5)
+priorityQueue.insert('打扫卫生', 1)
+priorityQueue.insert('做饭', 10)
+priorityQueue.insert('学习', 100)
 
-console.log(priorityQueue)
+console.log(priorityQueue.process())
+console.log(priorityQueue.process())
+console.log(priorityQueue.process())
