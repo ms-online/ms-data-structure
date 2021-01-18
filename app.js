1a0f0cad8cd11e3e947da43389dea2919fab0030
ms-online <27732357@qq.com>
2021-01-18 15:41:15 +0800

    lesson-52

--- app.js
+++ app.js
@@ -1,63 +1,36 @@
-class TrieNode {
-  constructor() {
-    this.value = null
-    this.children = Array(26)
+class Node {
+  constructor(value) {
+    this.value = value
+    this.next = null
   }
 }
 
-class Trie {
+class PriorityQueue {
   constructor() {
-    this.root = new TrieNode()
+    this.first = null
   }
+  //3,4,5
+  insert(value) {
+    const newNode = new Node(value)
 
-  insert(key, value) {
-    let node = this.root
-    for (let i = 0; i < key.length; i++) {
-      const letterIndext = key[i].charCodeAt(0) - 97
-      if (!node.children[letterIndext]) {
-        const newNode = new TrieNode()
-        node.children[letterIndext] = newNode
+    if (!this.first) {
+      this.first = newNode
+    } else {
+      let currentNode = this.first
+
+      while (currentNode.next) {
+        currentNode = currentNode.next
       }
-      node = node.children[letterIndext]
-    }
-    node.value = value
-  }
 
-  find(key) {
-    let node = this.root
-    for (let i = 0; i < key.length; i++) {
-      const letterIndext = key[i].charCodeAt(0) - 97
-
-      if (!node.children[letterIndext]) {
-        return false
-      }
-      node = node.children[letterIndext]
+      currentNode.next = newNode
     }
-    if (node.value === null) {
-      return false
-    }
-    return node
-  }
-
-  remove(key) {
-    const node = this.find(key)
-    node.value = null
   }
 }
 
-const trie = new Trie()
+const priorityQueue = new PriorityQueue()
 
-trie.insert('age', 25)
-trie.insert('name', 'summer')
-trie.insert('names', ['summer', 'henry'])
+priorityQueue.insert(3)
+priorityQueue.insert(4)
+priorityQueue.insert(5)
 
-console.log(trie)
-
-trie.remove('age')
-trie.remove('name')
-console.log(trie)
-
-console.log(trie.find('age'))
-console.log(trie.find('name'))
-console.log(trie.find('names'))
-console.log(trie.find('email'))
+console.log(priorityQueue)
